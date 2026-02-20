8f3e9dd1d7a7255577ce583f53c35197fa32ac80
MOHAMED ANAS M <btech.anas757ece@gmail.com>
2026-02-20 23:40:14 +0530

    Add files via upload

---
+++ hls_dsp_builtins.h
@@ -0,0 +1,51 @@
+// Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+// Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
+
+// 67d7842dbbe25473c3c32b93c0da8047785f30d78e8a024de1b57352245f9689
+/*
+ * __VIVADO_HLS_COPYRIGHT-INFO__
+ *
+ *
+ */
+
+#ifndef X_DSP_BUILTINS_H
+#define X_DSP_BUILTINS_H
+#include <stdint.h>
+
+#ifdef __cplusplus
+#include <ap_int.h>
+
+namespace hls {
+#ifdef __DSP58_PRIMARY__
+namespace dsp58 {
+typedef ap_int<58> C_t;
+typedef ap_int<27> A_t;
+typedef ap_int<24> B_t;
+};
+#endif
+
+#if defined __DSP48E1__ || defined __DSP48E2__ || defined __DSP58_PRIMARY__
+namespace dsp48e1 {
+typedef ap_int<48> C_t;
+typedef ap_int<25> A_t;
+typedef ap_int<18> B_t;
+};
+#endif
+
+#if defined __DSP48E2__ || defined __DSP58_PRIMARY__
+namespace dsp48e2 {
+typedef ap_int<48> C_t;
+typedef ap_int<27> A_t;
+typedef ap_int<18> B_t;
+};
+#endif
+};
+
+#endif
+
+#include "etc/dsp_functions.h"
+
+#endif
+
+
+
